5c2219c1ac3bb0dde4829eb6a8d5f7c78f9511cb
Andy Tran <aktran321@gmail.com>
2020-01-09 01:24:44 -0500

    Updates

---
+++ precache-manifest.16d40f3ed5b5e8f564e7b234fc63019e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c832a97ff647a01630685ecf9ec0ead",
+    "url": "/memory2/index.html"
+  },
+  {
+    "revision": "ebbe9b543898cb3c0d90",
+    "url": "/memory2/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1f8ef11defcb3558664e",
+    "url": "/memory2/static/js/2.9d2a95c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/memory2/static/js/2.9d2a95c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ebbe9b543898cb3c0d90",
+    "url": "/memory2/static/js/main.a50bb9f9.chunk.js"
+  },
+  {
+    "revision": "d578b36d56649a228487",
+    "url": "/memory2/static/js/runtime-main.c196341e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/memory2/static/media/logo.5d5d9eef.svg"
+  }
+]);
